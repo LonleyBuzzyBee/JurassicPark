bf9b4b1a6f673b39692200ad43effb5ef8490cba
mai cook <mailiecook@yahoo.com>
2020-06-08 10:08:27 -0700

    initialize project repo

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using JurassicPark.Models;
+
+namespace JurassicPark.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private JurassicParkContext _db;
+
+    public AnimalsController(JurassicParkContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get()
+    {
+      return _db.Animals.ToList();
+    }
+
+    // POST api/animals
+    [HttpPost]
+    public void Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+    }
+  }
+}
